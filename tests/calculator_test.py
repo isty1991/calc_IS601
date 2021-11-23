0b437fd1fc97f7dfacf538eeab86c80391bc831a
it55 <it55@njit.edu>
2021-11-23 12:53:52 -0500

    new changes

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -11,29 +11,32 @@ def clear_history_fixture():
     Calculations.clear_history()
 
 
+@pytest.fixture
+def test_tuple():
+    my_tuple = (1.0, 2.0, 5.0)
+    return my_tuple
+
+
 # You have to add the fixture function as a parameter to the test that you want to use it with
-def test_calculator_add_static(clear_history_fixture):
+def test_calculator_add_static(clear_history_fixture, test_tuple):
     """testing that our calculator has a static method for addition"""
     # pylint: disable=unused-argument,redefined-outer-name
     # using Tuple instead of args because we can pack as much data as we need into the tuple
-    my_tuple = (1.0, 2.0, 5.0)
-    Calculator.add_numbers(my_tuple)
+    Calculator.add_numbers(test_tuple)
     assert Calculator.get_result_value() == 8.0
 
 
-def test_calculator_subtract_static(clear_history_fixture):
+def test_calculator_subtract_static(clear_history_fixture, test_tuple):
     """Testing the subtract method of the calc"""
     # pylint: disable=unused-argument,redefined-outer-name
     # using Tuple instead of args because we can pack as much data as we need into the tuple
-    my_tuple = (1.0, 2.0, 3.0)
-    Calculator.subtract_numbers(my_tuple)
-    assert Calculator.get_result_value() == -6.0
+    Calculator.subtract_numbers(test_tuple)
+    assert Calculator.get_result_value() == -8.0
 
 
-def test_calculator_multiply_static(clear_history_fixture):
+def test_calculator_multiply_static(clear_history_fixture, test_tuple):
     """Testing the subtract method of the calc"""
     # pylint: disable=unused-argument,redefined-outer-name
     # using Tuple instead of args because we can pack as much data as we need into the tuple
-    my_tuple = (1.0, 2.0, 3.0)
-    Calculator.multiply_numbers(my_tuple)
-    assert Calculator.get_result_value() == 6.0
+    Calculator.multiply_numbers(test_tuple)
+    assert Calculator.get_result_value() == 10.0
