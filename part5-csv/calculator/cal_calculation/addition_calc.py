636dad2376a78d1a6e1683f15b16aa0d7dbdc2fa
isty1991 <90286703+isty1991@users.noreply.github.com>
2021-12-17 17:38:07 -0500

    Add files via upload

---
+++ part5-csv/calculator/cal_calculation/addition_calc.py
@@ -0,0 +1,14 @@
+""" Import Calculation Parent Class Constructor """
+from calculator.cal_calculation.calculation import Calculation
+# pylint: disable=too-few-public-methods
+
+
+class Addition(Calculation):
+    """ Performs addition between two values  """
+
+    def getresult(self):
+        """ Using self to reference the data contained in the object instance """
+        sum_of_values = 0.0
+        for value in self.values:
+            sum_of_values = sum_of_values + value
+        return sum_of_values
